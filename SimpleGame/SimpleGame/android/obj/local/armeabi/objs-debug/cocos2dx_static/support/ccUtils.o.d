3de0722f1f819e325d7f36a76832c2460cf6c2e4
Guanghui Qu <guanghui8827@126.com>
2012-02-22 22:32:57 +0800

    add libs & obis to .gitignore
    Signed-off-by: Guanghui Qu <guanghui8827@126.com>

--- SimpleGame/SimpleGame/android/obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_static/support/ccUtils.o: \
- jni/../../libs/cocos2dx/support/ccUtils.cpp \
- jni/../../libs/cocos2dx/support/ccUtils.h
-
-jni/../../libs/cocos2dx/support/ccUtils.h:
