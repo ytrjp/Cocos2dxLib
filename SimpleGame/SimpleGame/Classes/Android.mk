7e7df795ebfcc1fb3d886656b516eb58fd1318f8
Guanghui Qu <guanghui8827@126.com>
2012-02-23 16:37:38 +0800

    finished
    Signed-off-by: Guanghui Qu <guanghui8827@126.com>

--- SimpleGame/SimpleGame/Classes/Android.mk
+++ SimpleGame/SimpleGame/Classes/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := game_logic
+LOCAL_MODULE := game_logic_static
 
 LOCAL_SRC_FILES := AppDelegate.cpp \
                    HelloWorldScene.cpp
@@ -10,8 +10,15 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libs/cocos2dx \
                     $(LOCAL_PATH)/../libs/cocos2dx/include \
                     $(LOCAL_PATH)/../libs/CocosDenshion/include \
 
-LOCAL_LDLIBS := -L$(call host-path, $(LOCAL_PATH)/../android/libs/$(TARGET_ARCH_ABI)) \
-                -lcocosdenshion -lcocos2d  \
-                -L$(call host-path, $(LOCAL_PATH)/../libs/cocos2dx/platform/third_party/android/libraries/$(TARGET_ARCH_ABI)) 
+LOCAL_STATIC_LIBRARIES := png_static_prebuilt
+LOCAL_STATIC_LIBRARIES += xml2_static_prebuilt
+LOCAL_STATIC_LIBRARIES += jpeg_static_prebuilt
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
+                
+LOCAL_SHARED_LIBRARIES := cocosdenshion_shared
+            
+include $(BUILD_STATIC_LIBRARY)
 
-include $(BUILD_SHARED_LIBRARY)
+$(call import-module,cocos2dx/platform/third_party/android/modules/libpng)
+$(call import-module,cocos2dx/platform/third_party/android/modules/libxml2)
+$(call import-module,cocos2dx/platform/third_party/android/modules/libjpeg)
